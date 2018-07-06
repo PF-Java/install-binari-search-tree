65b3f5dd1f70fa5f0391d401872beeef2690fa86
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-07-06 11:21:16 +0700

    add interface Tree

---
+++ src/main/java/Tree/Tree.java
@@ -0,0 +1,21 @@
+package Tree;
+
+public interface Tree<E> {
+//    public boolean search(E e);
+
+    public boolean insert(E e);
+
+//    public boolean delete(E e);
+
+    public void inorder();
+
+//    public void preorder();
+
+//    public void postorder();
+
+    public int getSize();
+
+//    public boolean isEmpty();
+
+//    public void clear();
+}
